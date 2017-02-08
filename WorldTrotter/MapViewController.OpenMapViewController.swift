82a9a85378d2cc973cedfc79880261c4cd0e1d66
Jayden Olsen <Jayden@Jaydens-Mac-mini.local>
2017-02-07 22:06:23 -0500

    World Trotter after project 5, no Silver challenge

---
+++ WorldTrotter/MapViewController.OpenMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.OpenMapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jayden Olsen on 2/7/17.
+//  Copyright © 2017 Jayden Olsen. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
+
+
+
