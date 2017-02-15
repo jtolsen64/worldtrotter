1dfe7625da6bd70e318e14b1ca956b1a7c3ee9a1
Jayden Olsen <Jayden@Jaydens-Mac-mini.local>
2017-02-15 11:26:19 -0500

    Close to finishing challenges

--- WorldTrotter/MapViewController.OpenMapViewController.swift
+++ WorldTrotter/MapViewController.OpenMapViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import MapKit
-//import CoreLocation
+
 
 class MapViewController: UIViewController, MKMapViewDelegate,
     CLLocationManagerDelegate {
     
     var mapView: MKMapView!
-
     let locationManager = CLLocationManager()
+    var lastLoc: CLLocationCoordinate2D!
+    var lastSpan: MKCoordinateSpan!
+    var pinButtonPushed: Bool = false
+    var myLocButtonPushed: Bool = false
+    var pins: [MKPointAnnotation]=[]
+    var currentPin = 0
     
     override func loadView() {
         // Create a map view
@@ -26,7 +31,8 @@ class MapViewController: UIViewController, MKMapViewDelegate,
         //Set it as the view of this view controller
         view = mapView
         
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite", "My Location"])
+        //make segmented control
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex=0
         
@@ -37,16 +43,50 @@ class MapViewController: UIViewController, MKMapViewDelegate,
         segmentedControl.translatesAutoresizingMaskIntoConstraints=false
         view.addSubview(segmentedControl)
         
-        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor,
+        let scTopConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor,
                                                                   constant: 8)
         
         let margins = view.layoutMarginsGuide
-        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
-        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        let scLeadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let scTrailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
         
-        topConstraint.isActive = true
-        leadingConstraint.isActive = true
-        trailingConstraint.isActive = true
+        scTopConstraint.isActive = true
+        scLeadingConstraint.isActive = true
+        scTrailingConstraint.isActive = true
+        
+        //make my location button
+        let myLocButton = UIButton()
+        myLocButton.setTitle("My Location", for: .normal)
+        myLocButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        myLocButton.addTarget(self,
+                              action: #selector(MapViewController.showMyLocation),
+                                   for: .touchUpInside)
+        myLocButton.translatesAutoresizingMaskIntoConstraints=false
+        view.addSubview(myLocButton)
+        
+        let lbTopConstraint = myLocButton.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+                                                                  constant: -80)
+        
+        let lbTrailingConstraint = myLocButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        lbTopConstraint.isActive = true
+        lbTrailingConstraint.isActive = true
+        
+        //make pins button
+        let pinButton = UIButton()
+        pinButton.setTitle("Pins", for: .normal)
+        pinButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        pinButton.addTarget(self,
+                            action: #selector(MapViewController.cyclePins),
+                              for: .touchUpInside)
+        pinButton.translatesAutoresizingMaskIntoConstraints=false
+        view.addSubview(pinButton)
+        
+        let pbBotConstraint = pinButton.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+                                                               constant: -80)
+        
+        let pbLeadingConstraint = pinButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        pbBotConstraint.isActive = true
+        pbLeadingConstraint.isActive = true
     }
     
     override func viewDidLoad() {
@@ -63,18 +103,49 @@ class MapViewController: UIViewController, MKMapViewDelegate,
             mapView.mapType = .hybrid
         case 2:
             mapView.mapType = .satellite
-            self.mapView.showsUserLocation = false
-        case 3:
-            self.mapView.showsUserLocation = true
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestAlwaysAuthorization()
-            
-            locationManager.startUpdatingLocation()
         default:
             break
         }
     }
+    
+    func showMyLocation()
+    {
+        self.mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func cyclePins()
+    {
+        lastLoc = self.mapView.centerCoordinate
+        lastSpan = self.mapView.region.span
+        
+        //Set pins
+        //Pin 1: Born: Salt Lake City
+        let pin1 = MKPointAnnotation()
+        let coord1 = CLLocationCoordinate2D(latitude: , longitude: )
+        pin1.coordinate = coord1
+        mapView.addAnnotation(pin1)
+        pins.append(pin1)
+        
+        //Pin 2: Currently at: High Point
+        let pin2 = MKPointAnnotation()
+        let coord2 = CLLocationCoordinate2D(latitude: , longitude: )
+        pin2.coordinate = coord2
+        mapView.addAnnotation(pin2)
+        pins.append(pin2)
+        
+        //Pin 3: Interesting place: ?
+        let pin3 = MKPointAnnotation()
+        let coord3 = CLLocationCoordinate2D(latitude: , longitude: )
+        pin3.coordinate = coord3
+        mapView.addAnnotation(pin3)
+        pins.append(pin3)
+        
+    }
+    
     
     func mapViewWillStartLocatingUser(_ mapView: MKMapView)
     {
@@ -85,7 +156,6 @@ class MapViewController: UIViewController, MKMapViewDelegate,
     {
         print("Stop loading")
     }
-    
 }
 
 
